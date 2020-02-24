d49db84da6db3c23c1fdf2a6d64f3b1b735578f0
niccholaspage <niccholaspage@gmail.com>
2020-02-23 19:15:30 -0500

    Add comments to main class

--- BST/Main.cpp
+++ BST/Main.cpp
@@ -20,42 +20,64 @@ using namespace std;
 
 bool doesCommandRequireArgument(string command)
 {
+	// This helper method just checks if the given command
+	// requires an argument to function properly.
+	//
 	return command == "insert" || command == "delete" || command == "search" || command == "next" || command == "prev"
 		|| command == "parent" || command == "child";
 }
 
 void printHelp()
 {
-	cout << "insert <string>" << endl;
-	cout << "delete <string>" << endl;
-	cout << "search <string>" << endl;
-	cout << "min" << endl;
-	cout << "max" << endl;
-	cout << "next <string>" << endl;
-	cout << "prev <string>" << endl;
-	cout << "list" << endl;
-	cout << "parent <string>" << endl;
-	cout << "child <string>" << endl;
-	cout << "help" << endl;
-	cout << "quit" << endl;
+	// This method prints out a small description of the program as well as the commands
+	// available to the user as reference for using the program.
+	//
+	cout << "This program implements a Binary Search Tree data type with commands to manipulate and print out nodes of the tree.\n";
+	cout << "Available Commands:\n";
+	cout << "insert <string>\n";
+	cout << "delete <string>\n";
+	cout << "search <string>\n";
+	cout << "min\n";
+	cout << "max\n";
+	cout << "next <string>\n";
+	cout << "prev <string>\n";
+	cout << "list\n";
+	cout << "parent <string>\n";
+	cout << "child <string>\n";
+	cout << "help\n";
+	cout << "quit\n";
 }
 
 int main()
 {
-	BST* bst = new BST();
+	// This is the entry point of the application, so we construct the binary search
+	// tree and start the command loop to accept commands from the user to manipulate
+	// the tree.
+	//
+	BST* bst = new BST(); // We instantiate the tree we will work with
 
-	while (true)
+	while (true) // We loop forever, as we will be accepting commands for the lifetime of the program
 	{
-		string command;
+		string command; // Create a variable to store the command.
 
-		cin >> command;
+		cin >> command; // We pipe console input into the command.
 
+		// Since commands are case insensitive, we loop through all of the
+		// characters in the command,
 		for (unsigned int i = 0; i < command.length(); i++)
 		{
-			command[i] = tolower(command[i]);
+			command[i] = tolower(command[i]); // and we set each character to the lowercase version of itself.
 		}
 
 
+		// We now have a large if statement to dispatch commands without arguments
+		// to our tree. If we wanted to do this in a different possibly cleaner way,
+		// we could have a class for each command that implements a Command interface,
+		// and register commands in a map type structure, with the key being the word
+		// of the command, and the value being an instance of the class. Obviously,
+		// that is very complex for a project like this, especially considering we
+		// don't need to get into language features like that for this project.
+		//
 		if (command == "min")
 		{
 			bst->min();
@@ -74,14 +96,19 @@ int main()
 		}
 		else if (command == "quit")
 		{
+			// For the quit command, we are not just dispatching something to the tree.
+			// Instead, we are using a break statement, which will actually break us out
+			// of the while loop and continue execution of the remaning lines of code.
 			break;
 		}
-		else if (doesCommandRequireArgument(command))
+		else if (doesCommandRequireArgument(command)) // If the command requires an argument,
 		{
-			string argument;
+			string argument; // we create a variable for the argument,
 
-			cin >> argument;
+			cin >> argument; // and we pipe console input into the argument.
 
+			// We now have another large if statement to dispatch commands with arguments
+			// to our tree.
 			if (command == "insert")
 			{
 				bst->insert(argument);
@@ -113,7 +140,10 @@ int main()
 		}
 	}
 
-	delete bst;
+	// At this point, we have broken out of the while loop
+	// because the quit command has been given.
+	//
+	delete bst; // We delete the tree before ending the program
 
-	return 0;
+	return 0; // We return 0, as we have not experienced an error, and our program has exited!
 }
